ad74e1e84f46cf0cb3b58bf4839cf981733a624e
ZeroLive <zerolive@gmail.com>
2015-10-08 16:14:05 +0200

    Exercises about signup done

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    assert_not flash.nil?
+
   end
 
 end
